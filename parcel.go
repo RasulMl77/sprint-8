1e800f8db4e37aae8a3e0ffc0a7dcca808606d15
RasulMl77 <rasul0695@list.ru>
2026-01-03 14:11:15 +0300

    Добавил комментарии

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -40,7 +41,7 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 	// реализуйте чтение строки по заданному number
 	// здесь из таблицы должна вернуться только одна строка
 	row := s.db.QueryRow(
-		`SELECT number,client,status,address,created_ad FROM parcel WHERE number = ?`, number)
+		`SELECT number,client,status,address,created_at FROM parcel WHERE number = ?`, number)
 
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
@@ -48,7 +49,7 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 	if err != nil {
 		return Parcel{}, err
 	}
-	// если ошибка — возвращаем пустую посылку и ошибку }
+	// если ошибка — возвращаем пустую посылку и ошибку
 
 	return p, nil
 }
@@ -101,13 +102,53 @@ func (s ParcelStore) SetStatus(number int, status string) error {
 func (s ParcelStore) SetAddress(number int, address string) error {
 	// реализуйте обновление адреса в таблице parcel
 	// менять адрес можно только если значение статуса registered
-
+	query := `
+	 UPDATE parcel
+	 SET address = ?
+	 WHERE number = ? AND status = ?
+	 `
+	result, err := s.db.Exec(query, address, number, ParcelStatusRegistered)
+	if err != nil {
+		// Если произошла ошибка при выполнении запроса — возвращаем её.
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("нельзя изменить адрес: посылка не найдена или её статус не 'registered'")
+	}
+	// Всё прошло успешно.
 	return nil
 }
 
 func (s ParcelStore) Delete(number int) error {
-	// реализуйте удаление строки из таблицы parcel
-	// удалять строку можно только если значение статуса registered
+	// SQL-запрос: удаляем строку только если статус = registered
+	query := `
+        DELETE FROM parcel
+        WHERE number = ? AND status = ?
+    `
 
+	// Выполняем запрос
+	result, err := s.db.Exec(query, number, ParcelStatusRegistered)
+	if err != nil {
+		return err
+	}
+
+	// Узнаём, сколько строк реально удалилось
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// Если 0 — значит:
+	// - либо нет такой посылки
+	// - либо статус не registered
+	if rowsAffected == 0 {
+		return fmt.Errorf("нельзя удалить: посылка не найдена или её статус не 'registered'")
+	}
+
+	// Всё прошло успешно
 	return nil
 }
